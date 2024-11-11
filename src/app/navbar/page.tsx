1526d9bb97da21c60f3ba93fd1eb45cc280c3d4d
dat.haquoc <dat.haquoc@ncc.asia>
2024-11-11 16:24:45 +0700

    change app route

---
+++ src/app/navbar/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function Page() {
+  return (
+    <div>
+      <p>Nav Page </p>
+      <Link href="/category">category</Link>
+    </div>
+  );
+}
---
+++ src/app/post/page.tsx
@@ -0,0 +1,10 @@
+import ClientComponent from "@/component/ClientComponent";
+
+export default function Page() {
+  return (
+    <div>
+      <p>Post Page </p>
+      <ClientComponent />
+    </div>
+  );
+}
