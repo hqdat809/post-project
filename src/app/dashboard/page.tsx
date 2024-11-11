5b030e3b5edab7b05c2d95d6b7b674067ca91906
dat.haquoc <dat.haquoc@ncc.asia>
2024-11-11 17:34:34 +0700

    dynamic test

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import ClientComponent from "@/component/ClientComponent";
+import { unstable_noStore } from "next/cache";
 
 export const dynamicParams = true;
 
 export default function Page() {
+  unstable_noStore();
+
   return (
     <div>
       <p>{new Date().toUTCString()}</p>
