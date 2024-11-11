981fa59abca44497c2817df0d690b460f4ad5eb6
dat.haquoc <dat.haquoc@ncc.asia>
2024-11-11 14:19:07 +0700

    SSG2

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,27 @@
+import SSGComponent from "@/components/SSGComponent";
+import type { InferGetStaticPropsType, GetStaticProps } from "next";
+
+type Repo = {
+  name: string;
+  stargazers_count: number;
+};
+
+export const getStaticProps = (async () => {
+  const res = await fetch("https://api.github.com/repos/vercel/next.js");
+  const repo = await res.json();
+  console.log(repo);
+  return { props: { repo } };
+}) satisfies GetStaticProps<{
+  repo: Repo;
+}>;
+
+export default function Page({
+  repo,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div>
+      <p>Profile Page {repo.stargazers_count}</p>
+      <SSGComponent repo={repo} />
+    </div>
+  );
+}
