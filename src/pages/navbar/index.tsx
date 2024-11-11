d2f00b93dc4839321f845aaf894705d747584bcf
dat.haquoc <dat.haquoc@ncc.asia>
2024-11-11 16:10:12 +0700

    app route

--- src/pages/navbar/index.tsx
+++ src/pages/navbar/index.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 export default function Page() {
+  const router = useRouter();
   return (
     <div>
       <p>Nav Page </p>
       <Link href="/category">category</Link>
+      <button type="button" onClick={() => router.push("/dashboard")}>
+        Dashboard
+      </button>
     </div>
   );
 }
