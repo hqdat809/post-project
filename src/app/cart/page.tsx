e3771520afcd01360d99799128968411fa3fbf57
dat.haquoc <dat.haquoc@ncc.asia>
2024-11-12 09:00:46 +0700

    convert cart to ssr

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,8 @@
-"use client";
+export default async function Page() {
+  const res = await fetch("https://api.github.com/repos/vercel/next.js", {
+    cache: "no-store",
+  });
+  const products = await res.json();
 
-export default function Page() {
-  return <>Cart pages</>;
+  return <>Cart pages {products.name}</>;
 }
